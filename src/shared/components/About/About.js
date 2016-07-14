5e20c76b7e12b7e0c541dddfaabb4f55fdf60153
Sebastian Werner <s.werner@sebastian-software.de>
2016-07-14 12:51:48 +0200

    Updated author references, titles and urls.

--- src/shared/components/About/About.js
+++ src/shared/components/About/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div style={{ textAlign: 'center' }}>
       Produced with ❤️
-      by <a href="https://twitter.com/controlplusb" target="_blank">Sean Matheson</a>
+      by <a href="https://twitter.com/sebastiansoft" target="_blank">Sebastian Software</a>
     </div>
   );
 }
