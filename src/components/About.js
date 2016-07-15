12a0315ed97054d80d4eb7560a6bb208558cdd49
Sebastian Werner <s.werner@sebastian-software.de>
2016-07-15 16:08:30 +0200

    Added Markdown based HTML rendering. This is pretty freaking amazing ;)

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Button from "./Button"
+import Markdown from "react-markdown"
 import Styles from "./About.css"
 
 function About() {
@@ -9,6 +10,7 @@ function About() {
         Produced with ❤️ by <a href="https://github.com/sebastiansoft">Sebastian Software</a>
         <Button onClick={() => alert("clicked")}>Click me</Button>
       </p>
+      <Markdown source="# Hello from Markdown"></Markdown>
     </article>
   )
 }
