a522072059e8de191ba7fa08138c54510662ba7c
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-01 15:53:06 +0200

    Some fixes to new build tooling

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,10 @@
-var chalk = require("chalk");
-var path = require("path");
-var rimrafSync = require("rimraf").sync;
-var webpack = require("webpack");
+var chalk = require("chalk")
+var path = require("path")
+var rimrafSync = require("rimraf").sync
+var webpack = require("webpack")
+var series = require("async/series")
 
-var ConfigFactory = require("../webpack/ConfigFactory");
+var ConfigFactory = require("../webpack/ConfigFactory")
 var util = require("../webpack/util")
 
 const CWD = process.cwd()
@@ -12,30 +13,41 @@ const buildFolder = path.resolve(CWD, "build", "client")
 
 // Remove all content but keep the directory so that
 // if you're in it, you don't end up in Trash
-rimrafSync(buildFolder + '/*');
+rimrafSync(buildFolder + '/*')
 
 console.log("Creating an optimized production build for client...")
-const clientConfig = ConfigFactory({ target: "client", mode: "production", root: CWD }, {})
-webpack(clientConfig).run(function(err, stats)
-{
-  if (err)
+series(
+[
+  function(callback)
   {
-    console.error("Failed to create a production build. Reason:")
-    console.error(err.message || err)
-    process.exit(1)
-  }
+    const clientConfig = ConfigFactory({ target: "client", mode: "production", root: CWD }, {})
+    webpack(clientConfig).run(function(err, stats)
+    {
+      if (err)
+      {
+        console.error("Failed to create a production build. Reason:")
+        console.error(err.message || err)
+        process.exit(1)
+      }
 
-  util.logAssets(stats.toJson().assets, buildFolder)
-})
-
-console.log("Creating an production build for server...")
-const serverConfig = ConfigFactory({ target: "server", mode: "production", root: CWD }, {})
-webpack(serverConfig).run(function(err, stats)
-{
-  if (err)
+      util.logAssets(stats.toJson().assets, buildFolder)
+      callback()
+    })
+  },
+  function(callback)
   {
-    console.error("Failed to create a production build. Reason:")
-    console.error(err.message || err)
-    process.exit(1)
+    console.log("Creating an production build for server...")
+    const serverConfig = ConfigFactory({ target: "server", mode: "production", root: CWD }, {})
+    webpack(serverConfig).run(function(err, stats)
+    {
+      if (err)
+      {
+        console.error("Failed to create a production build. Reason:")
+        console.error(err.message || err)
+        process.exit(1)
+      }
+
+      callback()
+    })
   }
-})
+])
