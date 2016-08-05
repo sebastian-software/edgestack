af0ad4111c7f41a1d3f2f23ee1827abe3d21737f
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-05 13:08:01 +0200

    Enable compact code generation

--- config/babel.lib.js
+++ config/babel.lib.js
@@ -3,6 +3,9 @@ module.exports =
   // Don't try to find .babelrc because we want to force this configuration.
   babelrc: false,
 
+  // Faster transpiling for minor loose in formatting
+  compact: "auto",
+
   // Keep origin information alive
   sourceMaps: "inline",
 
