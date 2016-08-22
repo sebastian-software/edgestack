3c84826a28ac480e9ae3c7989191da18249c925c
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-22 12:07:01 +0200

    Added buitin modules list

--- webpack/ConfigFactory.js
+++ webpack/ConfigFactory.js
@@ -4,6 +4,7 @@ const path = require("path")
 const webpack = require("webpack")
 const AssetsPlugin = require("assets-webpack-plugin")
 const nodeExternals = require("webpack-node-externals")
+const builtinModules = require("builtin-modules")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 const BabelConfigClient = require("../config/babel.es.js")
