7488351ed4d8f4530267b8efab7fc345f9743d86
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-25 16:15:37 +0200

    Export config factory + disabled export of server code for now

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ export { default as HotServer } from "./webpack/HotServer"
 export { default as HotServers } from "./webpack/HotServers"
 export { default as ListenerManager } from "./webpack/ListenerManager"
 export { default as PostCSSConfig } from "./webpack/PostCSSConfig"
+export { default as ConfigFactory } from "./webpack/ConfigFactory"
 
-export { default as middleware } from "./server/middleware"
-export { default as server } from "./server/index"
+
+
+//export { default as middleware } from "./server/middleware"
+//export { default as server } from "./server/index"
