a791d4c6d55dedb1a18e82cd7d8131ca41e0a11d
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-25 14:06:28 +0200

    Added some useful exports for making this usable as a lib from external users

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as HotClient } from "./webpack/HotClient"
+export { default as HotServer } from "./webpack/HotServer"
+export { default as HotServers } from "./webpack/HotServers"
+export { default as ListenerManager } from "./webpack/ListenerManager"
+export { default as PostCSS } from "./webpack/PostCSS"
+
+export { default as middleware } from "./server/middleware"
+export { default as server } from "./server/index"
