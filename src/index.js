0dbada518e11fe50796edac03f7c8b5d7fa1012c
Sebastian Werner <s.werner@sebastian-software.de>
2016-09-02 15:04:29 +0200

    Export config reader + disabled export for overall server + middleware (both containing too project specific code)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export { default as ListenerManager } from "./webpack/ListenerManager"
 export { default as PostCSSConfig } from "./webpack/PostCSSConfig"
 export { default as ConfigFactory } from "./webpack/ConfigFactory"
 
+import * as ServerConfig from "./server/config"
+export { ServerConfig }
 
-
-//export { default as middleware } from "./server/middleware"
-//export { default as server } from "./server/index"
+export { default as generateServer } from "./server/factory"
