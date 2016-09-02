d1f5f0985347b84d5aad3d7023740273a2c12f11
Sebastian Werner <s.werner@sebastian-software.de>
2016-09-02 15:05:00 +0200

    Moved basic server creation into separate importable module

---
+++ src/server/factory.js
@@ -0,0 +1,55 @@
+import express from "express"
+import compression from "compression"
+import hpp from "hpp"
+import helmet from "helmet"
+
+import {
+  CLIENT_BUNDLE_HTTP_PATH,
+  CLIENT_BUNDLE_OUTPUT_PATH,
+  CLIENT_BUNDLE_CACHE_MAXAGE,
+  PUBLIC_DIR_PATH,
+} from "./config"
+
+export default function generateServer()
+{
+  // Create our express based server.
+  const app = express()
+
+  // Don't expose any software information to hackers.
+  app.disable("x-powered-by")
+
+  // Prevent HTTP Parameter pollution.
+  app.use(hpp())
+
+  // Content Security Policy
+  app.use(helmet.contentSecurityPolicy({
+    defaultSrc: [ "'self'" ],
+    scriptSrc: [ "'self'" ],
+    styleSrc: [ "'self'" ],
+    imgSrc: [ "'self'" ],
+    connectSrc: [ "'self'", "ws:" ],
+    fontSrc: [ "'self'" ],
+    objectSrc: [ "'none'" ],
+    mediaSrc: [ "'none'" ],
+    frameSrc: [ "'none'" ],
+  }))
+
+  app.use(helmet.xssFilter())
+  app.use(helmet.frameguard("deny"))
+  app.use(helmet.ieNoOpen())
+  app.use(helmet.noSniff())
+
+  // Response compression.
+  app.use(compression())
+
+  // Configure static serving of our webpack bundled client files.
+  app.use(
+    CLIENT_BUNDLE_HTTP_PATH,
+    express.static(CLIENT_BUNDLE_OUTPUT_PATH, { maxAge: CLIENT_BUNDLE_CACHE_MAXAGE })
+  )
+
+  // Configure static serving of our "public" root http path static files.
+  app.use(express.static(PUBLIC_DIR_PATH))
+
+  return app
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,63 +1,14 @@
-import express from "express"
-import compression from "compression"
-import hpp from "hpp"
-import helmet from "helmet"
 import universalReactAppMiddleware from "./middleware"
+import { SERVER_PORT } from "./config"
+import generateServer from "./factory"
 
-import {
-  CLIENT_BUNDLE_HTTP_PATH,
-  CLIENT_BUNDLE_OUTPUT_PATH,
-  CLIENT_BUNDLE_CACHE_MAXAGE,
-  SERVER_PORT,
-  PUBLIC_DIR_PATH,
-} from "./config"
-
-// Create our express based server.
-const app = express()
-
-// Don't expose any software information to hackers.
-app.disable("x-powered-by")
-
-// Prevent HTTP Parameter pollution.
-app.use(hpp())
-
-// Content Security Policy
-app.use(helmet.contentSecurityPolicy({
-  defaultSrc: [ "'self'" ],
-  scriptSrc: [ "'self'" ],
-  styleSrc: [ "'self'" ],
-  imgSrc: [ "'self'" ],
-  connectSrc: [ "'self'", "ws:" ],
-  fontSrc: [ "'self'" ],
-  objectSrc: [ "'none'" ],
-  mediaSrc: [ "'none'" ],
-  frameSrc: [ "'none'" ],
-}))
-
-app.use(helmet.xssFilter())
-app.use(helmet.frameguard("deny"))
-app.use(helmet.ieNoOpen())
-app.use(helmet.noSniff())
-
-// Response compression.
-app.use(compression())
-
-// Configure static serving of our webpack bundled client files.
-app.use(
-  CLIENT_BUNDLE_HTTP_PATH,
-  express.static(CLIENT_BUNDLE_OUTPUT_PATH, { maxAge: CLIENT_BUNDLE_CACHE_MAXAGE })
-)
-
-// Configure static serving of our "public" root http path static files.
-app.use(express.static(PUBLIC_DIR_PATH))
+const app = generateServer()
 
 // Bind our universal react app middleware as the handler for all get requests.
 app.get("*", universalReactAppMiddleware)
 
 // Create an http listener for our express app.
 const listener = app.listen(SERVER_PORT)
-
-console.log(`HTTP Listener is running on port ${SERVER_PORT}`) // eslint-disable-line no-console,max-len
 
 // We export the listener as it will be handy for our development hot reloader.
 export default listener
