8c28f312658cf06a8819f8e6252e3b2220f29440
Sebastian Werner <s.werner@sebastian-software.de>
2016-09-02 15:30:16 +0200

    Cleanup exports from webpack related stuff. This produces really large bundles when building servers elsewhere (using other exports from here)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-export { default as HotClient } from "./webpack/HotClient"
-export { default as HotServer } from "./webpack/HotServer"
-export { default as HotServers } from "./webpack/HotServers"
-export { default as ListenerManager } from "./webpack/ListenerManager"
-export { default as PostCSSConfig } from "./webpack/PostCSSConfig"
-export { default as ConfigFactory } from "./webpack/ConfigFactory"
-
 import * as ServerConfig from "./server/config"
 export { ServerConfig }
 
