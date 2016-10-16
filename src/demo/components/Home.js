1b15d8e89ee3bbbf388166983bb8a21683d89ea3
Sebastian Werner <s.werner@sebastian-software.de>
2016-10-16 18:35:00 +0200

    Added test files for local embeds of assets (which is somehow broken and needs fixing)

--- src/demo/components/Home.js
+++ src/demo/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Helmet from "react-helmet"
 
+import Styles from "./Home.css"
+
 function Home() {
   return (
     <article>
       <Helmet title="Home" />
+      <div className={Styles.preloader}></div>
       <p>
         Home Component
       </p>
