bf503e1c445c3909e67e3d8994cf3c35c39cb462
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-03 21:52:45 +0100

    Some demo cleanups

--- src/demo/components/About.js
+++ src/demo/components/About.js
@@ -8,10 +8,11 @@ function About() {
   return (
     <article>
       <Helmet title="About" />
+      <p>
+        <Button onClick={() => alert("clicked")}>Rehydrated Button</Button>
+      </p>
       <p className={Styles.intro}>
         Produced with ❤️ by <a href="https://github.com/sebastiansoft">Sebastian Software</a>
-        <Button onClick={() => alert("clicked")}>Click me</Button>
-        <Button onClick={() => alert("clicked")} disabled>Can't click me</Button>
       </p>
     </article>
   )
