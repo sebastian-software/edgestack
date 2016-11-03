8994d6143239221706aef2968cdfcb1fe6787b7d
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-03 11:11:50 +0100

    Corrected import/export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as ServerConfig from "./server/config"
 export { ServerConfig }
 
-export { default as generateServer } from "./server/factory"
+export { generateServer, addFallbackHandler } from "./server/factory"
 export { default as render } from "./server/render"
