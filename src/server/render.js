6a765eaa08cfb298893e2f1dd916254f88a4516c
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-08 21:49:12 +0100

    Reworked asset collection handling for new code split component

--- src/server/render.js
+++ src/server/render.js
@@ -2,7 +2,7 @@ import { renderToString } from "react-dom/server"
 import serialize from "serialize-javascript"
 import Helmet from "react-helmet"
 import { readFileSync } from "fs"
-
+import { STATE_IDENTIFIER } from 'code-split-component';
 import { ABSOLUTE_ASSETSINFO_PATH, ABSOLUTE_CHUNKMANIFEST_PATH } from "./config"
 
 var chunkManifest = "{}"
@@ -23,25 +23,23 @@ const clientAssets = JSON.parse(
 // javascript and css files.  Doing this is required as for production
 // configurations we add a hash to our filenames, therefore we won't know the
 // paths of the output by webpack unless we read them from the assets.json file.
-const chunks = Object.keys(clientAssets).map((key) => clientAssets[key])
-const assets = chunks.reduce((sorted, chunk) =>
-{
-  if (chunk.js) {
-    if (chunk.js.indexOf("/vendor-") !== -1) {
-      sorted.scripts.unshift(chunk.js)
-    } else {
-      sorted.scripts.push(chunk.js)
+// const chunks = Object.keys(clientAssets).map((key) => clientAssets[key])
+
+function getAssetsForClientChunks(chunks) {
+  return chunks.reduce((acc, chunkName) => {
+    const chunkAssets = clientAssets[chunkName];
+    if (chunkAssets) {
+      if (chunkAssets.js) {
+        acc.scripts.push(chunkAssets.js);
+      }
+      if (chunkAssets.css) {
+        acc.styles.push(chunkAssets.css);
+      }
     }
-  }
-  if (chunk.css) {
-    if (chunk.css.indexOf("/vendor-") !== -1) {
-      sorted.styles.unshift(chunk.css)
-    } else {
-      sorted.styles.push(chunk.css)
-    }
-  }
-  return sorted
-}, { scripts: [], styles: [] })
+    return acc;
+  }, { scripts: [], styles: [] });
+}
+
 
 
 /**
@@ -100,14 +98,32 @@ function serviceWorkerScript(nonce) {
  *
  * @return The full HTML page in the form of a React element.
  */
-export default function render({ app, initialState, nonce }) {
+export default function render({ app, initialState, nonce, helmet, codeSplitState }) {
   const appString = app ? renderToString(app) : null
 
-  // We run 'react-helmet' after our renderToString call so that we can fish
-  // out all the attributes which need to be attached to our page.
-  // React Helmet allows us to control our page header contents via our
-  // components.
-  const helmet = app ? Helmet.rewind() : null
+  // The chunks that we need to fetch the assets (js/css) for and then include
+  // said assets as script/style tags within our html.
+  const chunksForRender = [
+    // We always manually add the main entry chunk for our client bundle. It
+    // must always be the first item in the collection.
+    "vendor",
+    "main"
+  ];
+
+  if (codeSplitState) {
+    // We add all the chunks that our CodeSplitProvider tracked as being used
+    // for this render.  This isn't actually required as the rehydrate function
+    // of code-split-component which gets executed in our client bundle will
+    // ensure all our required chunks are loaded, but its a nice optimisation as
+    // it means the browser can start fetching the required files before it's
+    // even finished parsing our client bundle entry script.
+    // Having the assets.json file available to us made implementing this
+    // feature rather arbitrary.
+    codeSplitState.chunks.forEach(chunk => chunksForRender.push(chunk));
+  }
+
+  // Now we get the assets (js/css) for the chunks.
+  const assetsForRender = getAssetsForClientChunks(chunksForRender)
 
   return `<!doctype html>
     <html ${helmet ? helmet.htmlAttributes.toString() : ""}>
@@ -116,7 +132,7 @@ export default function render({ app, initialState, nonce }) {
         ${helmet ? helmet.meta.toString() : ""}
         ${helmet ? helmet.link.toString() : ""}
 
-        ${styleTags(assets.styles)}
+        ${styleTags(assetsForRender.styles)}
         ${helmet ? helmet.style.toString() : ""}
 
         ${serviceWorkerScript(nonce)}
@@ -126,10 +142,11 @@ export default function render({ app, initialState, nonce }) {
 
         <script nonce="${nonce}">${
           (initialState ? `window.APP_STATE=${serialize(initialState)};` : "") +
-          `window.CHUNK_MANIFEST=${chunkManifest};`
+          `window.CHUNK_MANIFEST=${chunkManifest};` +
+          `window.${STATE_IDENTIFIER}=${serialize(codeSplitState)};`
         }</script>
 
-        ${scriptTags(assets.scripts)}
+        ${scriptTags(assetsForRender.scripts)}
         ${helmet ? helmet.script.toString() : ""}
       </body>
     </html>`
