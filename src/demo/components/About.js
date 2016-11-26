f4dfa89df8d0d59b4ea70b07af840fa435d0622d
Sebastian Werner <s.werner@sebastian-software.de>
2016-11-26 22:10:06 +0100

    Worked on redux stuff to test integration with async data

--- src/demo/components/About.js
+++ src/demo/components/About.js
@@ -1,21 +1,61 @@
 import React from "react"
 import Helmet from "react-helmet"
+import { connect } from "react-redux"
 
 import Button from "./Button"
 import Styles from "./About.css"
 
-function About() {
-  return (
-    <article>
-      <Helmet title="About" />
-      <p>
-        <Button onClick={() => alert("clicked")}>Rehydrated Button</Button>
-      </p>
-      <p className={Styles.intro}>
-        Produced with ❤ by <a href="https://github.com/sebastian-software">Sebastian Software</a>
-      </p>
-    </article>
-  )
+class About extends React.Component {
+  static fetchData(props, context) {
+    if (props.updateTime) {
+      console.log("Fetching data for <About>...")
+      return new Promise((resolve, reject) => {
+        console.log(props.updateTime())
+        resolve()
+      })
+    }
+  }
+
+  handleClick() {
+    alert("Clicked!")
+  }
+
+  render() {
+    return(
+      <article>
+        <Helmet title="About" />
+        <p>
+          <Button onClick={this.handleClick}>Rehydrated Button</Button>
+        </p>
+        <p className={Styles.intro}>
+          Produced with ❤ by <a href="https://github.com/sebastian-software">Sebastian Software</a>
+        </p>
+      </article>
+    )
+  }
 }
 
-export default About
+const SET_TIME = "SET_TIME"
+
+function setTime(time) {
+  return {
+    type: SET_TIME,
+    now: time
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    // fill me
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    updateTime: () => {
+      dispatch(setTime(new Date()))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(About)
