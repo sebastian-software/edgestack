8a9023082081ba476053e29bde0c8012ab6299bd
Sebastian Werner <s.werner@sebastian-software.de>
2016-12-19 16:22:44 +0100

    Updated prepublish + added browser entry point

---
+++ src/browser.js
@@ -0,0 +1,3 @@
+// This file is just for exporting infrastructure to applications built upon this.
+export { createReduxStore, createApolloClient,
+  emptyReducer, emptyMiddleware, emptyEnhancer } from "./app/Data"
