1fafee71c1df32a6feeaed4c996c614988c8e84f
Sebastian Werner <s.werner@sebastian-software.de>
2017-02-01 12:09:08 +0100

    Added dom variant of react-router v4

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 // eslint-disable filenames/match-exported
 import React from "react"
 import { render } from "react-dom"
-import { BrowserRouter } from "react-router"
+import { BrowserRouter } from "react-router-dom"
 import { ApolloProvider } from "react-apollo"
 import { withAsyncComponents } from "react-async-component"
 
