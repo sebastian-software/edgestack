a44fe28d02d2e9cba6d864c89ec2f3f5359a02c1
Sebastian Werner <s.werner@sebastian-software.de>
2017-02-07 07:57:19 +0100

    Re-synced hot development code with react-universally

--- src/webpack/ListenerManager.js
+++ src/webpack/ListenerManager.js
@@ -1,69 +1,58 @@
+const { log } = require('../utils');
+
 class ListenerManager {
-  constructor(server) {
-    this.lastConnectionKey = 0
-    this.connectionMap = {}
-    this.started = false
+  constructor(listener, name) {
+    this.name = name || 'listener';
+    this.lastConnectionKey = 0;
+    this.connectionMap = {};
+    this.listener = listener;
 
-    server.then((listener) =>
-    {
-      this.started = true
-      this.listener = listener
-
-      // Track all connections to our server so that we can close them when needed.
-      this.listener.on("connection", (connection) => {
-        // Generate a new key to represent the connection
-        const connectionKey = ++this.lastConnectionKey
-
-        // Add the connection to our map.
-        this.connectionMap[connectionKey] = connection
-
-        // Remove the connection from our map when it closes.
-        connection.on("close", () => {
-          delete this.connectionMap[connectionKey]
-        })
-      })
-    })
+    // Track all connections to our server so that we can close them when needed.
+    this.listener.on('connection', (connection) => {
+      // Increment the connection key.
+      this.lastConnectionKey += 1;
+      // Generate a new key to represent the connection
+      const connectionKey = this.lastConnectionKey;
+      // Add the connection to our map.
+      this.connectionMap[connectionKey] = connection;
+      // Remove the connection from our map when it closes.
+      connection.on('close', () => {
+        delete this.connectionMap[connectionKey];
+      });
+    });
   }
 
   killAllConnections() {
     Object.keys(this.connectionMap).forEach((connectionKey) => {
-      this.connectionMap[connectionKey].destroy()
-    })
+      this.connectionMap[connectionKey].destroy();
+    });
   }
 
-  _tryDispose(force = false) {
-    return new Promise((resolve, reject) => {
-      if (force) {
-        // Forcefully close any existing connections.
-        this.killAllConnections()
-      }
-
-      // Close the listener.
+  dispose() {
+    return new Promise((resolve) => {
       if (this.listener) {
-        this.listener.close((err) => {
-          if (err) {
-            // reject when server is not open when it is closed
-            // see https://nodejs.org/api/net.html#net_server_close_callback
-            reject(err)
-          }
-          // Ensure no straggling connections are left over.
-          this.killAllConnections()
+        this.killAllConnections();
 
-          resolve()
-        })
+        log({
+          title: this.name,
+          level: 'info',
+          message: 'Destroyed all existing connections.',
+        });
+
+        this.listener.close(() => {
+          log({
+            title: this.name,
+            level: 'info',
+            message: 'Closed listener.',
+          });
+
+          resolve();
+        });
       } else {
-        resolve()
+        resolve();
       }
-    })
-  }
-
-  dispose(force = false) {
-    return this._tryDispose(force)
-      .catch(
-        // postpone 100ms to retry
-        () => new Promise((resolve) => setTimeout(resolve, 100)).then(() => this.dispose(force))
-      )
+    });
   }
 }
 
-export default ListenerManager
+export default ListenerManager;
