8c0f58a51ad469fbf12da244802ac6d16edcdd39
Sebastian Werner <s.werner@sebastian-software.de>
2017-02-15 14:41:13 +0100

    Minor tweaks for Intl support

--- src/common/Intl.js
+++ src/common/Intl.js
@@ -1,7 +1,10 @@
 import "isomorphic-fetch"
 import areIntlLocalesSupported from "intl-locales-supported"
 
+// TODO: Figure out whether we can use some local mirror from e.g. public path or
+// ideally via asset handling with content hashes, etc.
 const BASE_URL = "https://unpkg.com/lean-intl/locale-data/json/"
+const PREFER_NATIVE = false
 
 function getLanguage() {
   return typeof document !== "undefined" ? document.documentElement.lang : "de-DE" // FIXME
@@ -11,14 +14,13 @@ export function ensureIntlSupport() {
   const language = getLanguage().replace("_", "-")
 
   // Determine if the built-in `Intl` has the locale data we need.
-  if (global.Intl && areIntlLocalesSupported([ language ])) {
+  if (PREFER_NATIVE && global.Intl && areIntlLocalesSupported([ language ])) {
     return Promise.resolve(false)
   }
 
   return import("lean-intl").then((IntlPolyfill) => {
     return fetch(BASE_URL + language + ".json").then((response) => {
-
-      response.json().then((parsed) => {
+      return response.json().then((parsed) => {
         IntlPolyfill.__addLocaleData(parsed)
 
         // `Intl` exists, but it doesn't have the data we need, so load the
