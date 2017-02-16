1a5ec18e758642143af008dd08c66bf24b50aa97
Sebastian Werner <s.werner@sebastian-software.de>
2017-02-16 18:17:34 +0100

    Cleanup

--- src/app/Root.js
+++ src/app/Root.js
@@ -39,7 +39,6 @@ function Root({ children, locale, language }) {
             { name: "charset", content: "utf-8" },
             { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { name: "content-language", content: language },
             { name: "description", content: websiteDescription }
           ]}
         />
