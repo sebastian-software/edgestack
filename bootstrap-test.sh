30f7f320ec5cdb462162ee3d1dbef5efa4e5f670
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-02-18 21:18:05 +0100

    Fix initial package.json

--- bootstrap-test.sh
+++ bootstrap-test.sh
@@ -5,7 +5,7 @@ TESTPATH="${CWD}/test_boilerplate"
 
 mkdir -p ${TESTPATH}
 cd "${TESTPATH}"
-echo '{ "name":"test","version":"1.0.0", "devDependencies": { "cross-env": "^3.1.4"}, scripts":{"prod":"cross-env NODE_ENV=production advanced-script build"} }' > package.json
+echo '{ "name":"test","version":"1.0.0", "devDependencies": { "cross-env": "^3.1.4"}, "scripts":{"prod":"cross-env NODE_ENV=production advanced-script build"} }' > package.json
 
 npm install
 npm link ..
