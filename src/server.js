f32452d5cc0e2dc174b06e640fc87ec218c8a6f4
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-02-19 12:59:00 +0100

    Export gtLocale and getLanguage

--- src/server.js
+++ src/server.js
@@ -8,7 +8,14 @@ export { default as createExpressServer } from "./server/createExpressServer"
 export { default as createUniversalMiddleware } from "./server/createUniversalMiddleware"
 export { default as addFallbackHandler } from "./server/addFallbackHandler"
 export { default as renderPage } from "./server/renderPage"
-export { createReduxStore, emptyReducer, emptyMiddleware, emptyEnhancer } from "./common/State"
+export {
+  createReduxStore,
+  emptyReducer,
+  emptyMiddleware,
+  emptyEnhancer,
+  getLocale,
+  getLanguage
+} from "./common/State"
 export { createApolloClient } from "./common/Apollo"
 
 export { default as RouterConnector, routerReducer } from "./common/RouterConnector"
