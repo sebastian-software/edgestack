57e64632666c1d0b0ec3b97047a6a649e1e70c3c
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-02-23 01:35:50 +0100

    Update generated package.json

--- bootstrap-test.sh
+++ bootstrap-test.sh
@@ -6,7 +6,7 @@ TESTPATH="$(mktemp -d)"
 
 mkdir -p ${TESTPATH}
 cd "${TESTPATH}"
-echo '{ "name":"test","version":"1.0.0", "devDependencies": { "cross-env": "^3.1.4"}, "scripts":{"prod":"cross-env NODE_ENV=production edge build"} }' > package.json
+echo '{ "name":"test","version":"1.0.0", "devDependencies": { "cross-env": "^3.1.4" }, "scripts":{"prod":"cross-env NODE_ENV=production edge build"} }' > package.json
 
 npm -q install ${CWD}
 
