819e3326513e556fdf82e1792f0b154f20e9b998
Sebastian Fastner <s.fastner@sebastian-software.de>
2017-02-23 01:23:47 +0100

    Fix bootstrap test script

--- bootstrap-test.sh
+++ bootstrap-test.sh
@@ -8,7 +8,7 @@ mkdir -p ${TESTPATH}
 cd "${TESTPATH}"
 echo '{ "name":"test","version":"1.0.0", "devDependencies": { "cross-env": "^3.1.4"}, "scripts":{"prod":"cross-env NODE_ENV=production edge build"} }' > package.json
 
-npm -q install ..
+npm -q install ${CWD}
 
 node_modules/.bin/edge bootstrap --title="Test" --description="Test" --language="de-DE"
 npm -q install
