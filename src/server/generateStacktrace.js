309cfe3a2ff8f4c70e548496cafbda0f512b53a5
Sebastian Werner <s.werner@sebastian-software.de>
2017-03-13 11:32:41 +0100

    Minor code tweaks

--- src/server/generateStacktrace.js
+++ src/server/generateStacktrace.js
@@ -1,11 +1,12 @@
 import fs from "fs"
 import chalk from "chalk"
 import { wrapCallSite } from "source-map-support"
-import { startsWith } from "lodash"
+import { startsWith, repeat } from "lodash"
 import appRootDir from "app-root-dir"
 
 const LINES_AROUND_STACK_POSITION = 5
 const SHOW_EXTENDED_STACKTRACE = false
+const ROOT_DIR = appRootDir.get()
 
 function readFile(filename)
 {
@@ -21,40 +22,31 @@ function readFile(filename)
   })
 }
 
-function padding(characters)
-{
-  let result = ""
-  while (result.length < characters)
-    result += " "
-
-  return result
-}
-
-function getSourceContent({
+function generateMessage({
   filename,
   line,
-  col,
-  errorMessage,
+  column,
+  message,
   stackTrace
 })
 {
   return readFile(filename).then((wholeFileContent) => {
     const result = [
-      chalk.cyan(`Error in file ${filename} (${line}:${col})\n`)
+      chalk.red(`Error in file ${filename} (${line}:${column})\n`)
     ]
     const nullLine = line - 1
     const fileContent = wholeFileContent.split("\n")
     const startLine = Math.max(nullLine - LINES_AROUND_STACK_POSITION, 0)
     const endLine = Math.min(nullLine + LINES_AROUND_STACK_POSITION, fileContent.length)
 
-    for (let counter = startLine; counter < nullLine; counter++)
-    {
+    for (let counter = startLine; counter < nullLine; counter++) {
       result.push(chalk.gray(fileContent[counter]))
     }
-    result.push(chalk.red(fileContent[nullLine]))
-    result.push(chalk.cyan(`${padding(col - 1)}^--- Error: ${errorMessage}`))
-    for (let counter = nullLine + 1; counter < endLine; counter++)
-    {
+
+    result.push(chalk.blue(fileContent[nullLine]))
+    result.push(chalk.red(`${repeat(" ", column - 1)}^ Error: ${message}`))
+
+    for (let counter = nullLine + 1; counter < endLine; counter++) {
       result.push(chalk.gray(fileContent[counter]))
     }
 
@@ -65,7 +57,7 @@ function getSourceContent({
   })
 }
 
-function frameToString(frame, rootDir)
+function frameToString(frame)
 {
   const wrappedFrame = wrapCallSite(frame)
 
@@ -114,28 +106,23 @@ function frameToString(frame, rootDir)
   return {
     ...wrappedFrame,
     toParsed,
-    toString: () => chalk.dim(wrappedFrame.toString().replace(rootDir, "."))
+    toString: () => chalk.dim(wrappedFrame.toString().replace(ROOT_DIR, "."))
   }
 }
 
-function enableWebpackServerStacktrace()
+Error.prepareStackTrace = function prepareStackTrace(error, stack)
 {
-  const rootDir = appRootDir.get()
-  Error.prepareStackTrace = function prepareStackTrace(error, stack) {
-    const wrappedStack = stack.map((frame) => frameToString(frame, rootDir)).filter((item) => Boolean(item))
-    const toString = error + wrappedStack.map((frame) =>
-    {
-      return `\n    at ${frame}`
-    }).join("")
+  const wrappedStack = stack.map((frame) => frameToString(frame)).filter((item) => Boolean(item))
+  const toString = error + wrappedStack.map((frame) =>
+  {
+    return `\n  at ${frame}`
+  }).join("")
 
-    return {
-      toString: () => toString,
-      rawStack: () => wrappedStack
-    }
+  return {
+    toString: () => toString,
+    rawStack: () => wrappedStack
   }
 }
-
-enableWebpackServerStacktrace()
 
 export default async function generateStacktrace(error)
 {
@@ -144,11 +131,11 @@ export default async function generateStacktrace(error)
 
   const parsedStackRoot = error.stack.rawStack()[0].toParsed()
 
-  return await getSourceContent({
+  return await generateMessage({
     filename: parsedStackRoot.sourceFile,
     line: parsedStackRoot.lineNumber,
-    col: parsedStackRoot.columnNumber,
-    errorMessage: error.message,
+    column: parsedStackRoot.columnNumber,
+    message: error.message,
     stackTrace: error.stack
   })
 }
