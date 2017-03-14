43b639382ada9cab5ba6e37fa4eee44387bd2162
Sebastian Werner <s.werner@sebastian-software.de>
2017-03-14 18:24:52 +0100

    Major docs rework + cleanup

--- src/server/debug.js
+++ src/server/debug.js
@@ -1,9 +1,82 @@
+import codeFrame from "babel-code-frame"
+import chalk from "chalk"
 import { wrapCallSite } from "source-map-support"
+import { readFileSync } from "fs"
 
-const filterFunctionNames = [ /__webpack_require__/ ]
-const filterSourceFiles = [ /\/webpack\/bootstrap/ ]
-const clearFunctionNames = [ /__webpack_exports__/ ]
-const clearTypeNames = [ /^Object$/ ]
+const FUNCTION_NAME_FILTERS = [ /__webpack_require__/ ]
+const SOURCE_FILE_FILTERS = [ /\/webpack\/bootstrap/ ]
+
+const FUNCTION_NAME_CLEARANCE = [ /__webpack_exports__/ ]
+const TYPE_NAME_CLEARANCE = [ /^Object$/ ]
+
+const CODE_FRAME_OPTIONS = {
+  highlightCode: true
+}
+
+
+/**
+ * Cleans up the source file name which was modified by `source-map-support` to point to the source mapped
+ * origin file. Unfortunately instead of giving us the pure source file it resolves into something
+ * like "<generatedFile>:<sourceFile>". We take care of this here and remove the generated file segment
+ * as this is typically not relevant for the developer.
+ * @param {String} sourceFile Sourcefile as being available in the CallSite
+ * @return {String} Cleaned up plain source file reference
+ */
+export function cleanSourceFileName(sourceFile)
+{
+  var cleanFile = sourceFile.split(":")[1] || sourceFile
+  if (cleanFile.charAt(0) === "/") {
+    cleanFile = cleanFile.slice(1)
+  }
+
+  return cleanFile
+}
+
+
+/**
+ * This method checks whether the given stack frame (CallSite) is relevant from a
+ * developer perspective.
+ * @param {CallSite} frame Incoming frame to check for user relevance
+ * @return {Boolean} Whether the frame is relevant for end developer debugging.
+ */
+export function isRelevantFrame(frame)
+{
+  const wrappedFrame = wrapCallSite(frame)
+  const generatedFile = frame.getFileName()
+
+  var sourceFile = wrappedFrame.getFileName()
+  var functionName = wrappedFrame.getFunctionName()
+
+  // Filter out all raw files which do not have any source mapping
+  // This typically removes most of the build related infrastructure
+  // which is neither application or framework code.
+  const isCompiled = sourceFile !== generatedFile
+  if (!isCompiled) {
+    return false
+  }
+
+  // Filter out specific functions e.g. webpack require wrappers
+  if (FUNCTION_NAME_FILTERS.some((regexp) => regexp.test(functionName))) {
+    return false
+  }
+
+  // Filter out specific source files e.g. webpack bootstrap implementation
+  if (SOURCE_FILE_FILTERS.some((regexp) => regexp.test(sourceFile))) {
+    return false
+  }
+
+  return true
+}
+
+
+/**
+ * Filters the existing list of CallSites to drop non user-relevant ones.
+ * @param {CallSite[]} frames List of CallSite objects
+ * @return {CallSite[]} Filtered CallSite list
+ */
+export function getRelevantFrames(frames) {
+  return frames.filter(isRelevantFrame)
+}
 
 
 /**
@@ -16,82 +89,120 @@ const clearTypeNames = [ /^Object$/ ]
 export function frameToString(frame)
 {
   const wrappedFrame = wrapCallSite(frame)
-  const generatedFile = frame.getFileName()
 
   var sourceFile = wrappedFrame.getFileName()
   var functionName = wrappedFrame.getFunctionName()
   var typeName = wrappedFrame.getTypeName()
 
-  // Filter out all raw files which do not have any source mapping
-  // This typically removes most of the build related infrastructure
-  // which is neither application or framework code.
-  const isCompiled = sourceFile !== generatedFile
-  if (!isCompiled) {
-    return null
-  }
-
-  // Filter out specific functions e.g. webpack require wrappers
-  if (filterFunctionNames.some((regexp) => regexp.test(functionName))) {
-    return null
-  }
-
-  // Filter out specific source files e.g. webpack bootstrap implementation
-  if (filterSourceFiles.some((regexp) => regexp.test(sourceFile))) {
-    return null
-  }
-
   // Ignore some cryptic function names which typically are just function calls
-  if (clearFunctionNames.some((regexp) => regexp.test(functionName))) {
+  if (FUNCTION_NAME_CLEARANCE.some((regexp) => regexp.test(functionName))) {
     functionName = ""
   }
 
   // Filter out configured type names
-  if (clearTypeNames.some((regexp) => regexp.test(typeName))) {
+  if (TYPE_NAME_CLEARANCE.some((regexp) => regexp.test(typeName))) {
     typeName = ""
   }
 
   // Strip out generated filename part from source field
-  sourceFile = sourceFile.split(":")[1] || sourceFile
-  if (sourceFile.charAt(0) === "/") {
-    sourceFile = sourceFile.slice(1)
-  }
+  sourceFile = cleanSourceFileName(sourceFile)
 
   // Retrieve source file locations
   const lineNumber = wrappedFrame.getLineNumber()
   const columnNumber = wrappedFrame.getColumnNumber()
 
+  var identifier = functionName || typeName || "<anonymous>"
+
   // Stack frames are displayed in the following format:
   //   at FunctionName (<Fully-qualified name/URL>:<line number>:<column number>)
   // Via: https://docs.microsoft.com/en-us/scripting/javascript/reference/stack-property-error-javascript
-  return `at ${functionName || typeName}@${sourceFile}:${lineNumber}:${columnNumber}`
+  return `at ${identifier}@${sourceFile}:${lineNumber}:${columnNumber}`
 }
 
 
 /**
  * Build the stack property for V8 as V8 is documented to use whatever this call returns
  * as the value of the stack property.
- * @param {Error} error the error object
+ * @param {Error} nativeError Native JavaScript Error Object
  * @param {CallSite[]} structuredStackTrace a structured representation of the stack
  * @return {String} Generated `stack` property for error object
  */
-export function prepareStackTrace(error, structuredStackTrace)
+export function prepareStackTrace(nativeError, structuredStackTrace)
 {
-  return structuredStackTrace.map((frame) => frameToString(frame))
+  var frames = getRelevantFrames(structuredStackTrace)
+
+  var firstFrame = wrapCallSite(frames[0])
+  var sourceFile = cleanSourceFileName(firstFrame.getFileName())
+
+  var sourceText = ""
+  try {
+    sourceText = readFileSync(sourceFile, "utf-8")
+  } catch (error) {
+    // Ignore errors
+  }
+
+  // Generate highlighted code frame and attach it to the native error object (for later usage)
+  if (sourceText) {
+    const result = codeFrame(sourceText, firstFrame.getLineNumber(), firstFrame.getColumnNumber(), CODE_FRAME_OPTIONS)
+    nativeError.code = result
+  }
+
+  return frames.map((frame) => frameToString(frame))
     .filter((item) => item != null)
     .join("\n")
 }
 
 
 /**
+ * Highlights the given stacktrace object using `chalk
+ * @param {Stack} Stacktrace result string
+ * @return {String} Highlighted stack trace for NodeJS
+ */
+export function highlightStack(stack)
+{
+  return stack.split("\n").map((line) => {
+    if (line.startsWith("at "))
+    {
+      /* eslint-disable max-params */
+      return line.replace(/(at )(.*?)(@)(.*?):([0-9]+)(:)([0-9]+)/,
+        (match, intro, id, symbol, filename, lineNo, separator, columnNo) =>
+        `  - ${chalk.white(id)} ${chalk.dim(filename)} [${chalk.yellow(lineNo)}:${chalk.yellow(columnNo)}]`)
+    }
+
+    return chalk.red(line)
+  }).join("\n")
+}
+
+
+/**
+ * Logs the given error to the NodeJS console
+ * @param {Error} error Native JavaScript Error Object
+ * @return {[type]} [description]
+ */
+export function logError(error) {
+  /* eslint-disable no-console */
+  console.error(`${error.code}\n\n${highlightStack(error.stack)}`)
+}
+
+
+/**
  * Enable enhanced stack traces
  */
-export function enableEnhancedStackTraces() {
+export function enableEnhancedStackTraces()
+{
   // Override native Promise API with faster and more developerr friendly bluebird
   global.Promise = require("bluebird")
 
   /* eslint-disable no-use-extend-native/no-use-extend-native */
   Promise.config({
     longStackTraces: true
+  })
+
+  // Catch unhandled NodeJS error as pass them over to our log handler
+  process.on("unhandledRejection", (reason, promise) => {
+    if (reason instanceof Error) {
+      logError(reason)
+    }
   })
 
   // Enable by hooking into V8 Stacktrace API integration
