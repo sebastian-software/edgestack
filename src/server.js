fc699af17941d6ec58b4589ae0dffe80394afbfa
Sebastian Werner <s.werner@sebastian-software.de>
2017-03-15 15:09:22 +0100

    Do not export full config... working on alternative approach

--- src/server.js
+++ src/server.js
@@ -1,9 +1,5 @@
 // This file is just for exporting infrastructure to applications built upon this.
 
-// eslint-disable-next-line import/no-namespace
-import * as ServerConfig from "./api/server/config"
-export { ServerConfig }
-
 export * from "./api/common"
 
 export { default as createExpressServer } from "./api/server/createExpressServer"
