acffb29c0d8c2527c6731320a257ef1080774d07
Sebastian Werner <s.werner@sebastian-software.de>
2017-03-15 13:57:18 +0100

    Cleanups

--- src/server/index.js
+++ src/server/index.js
@@ -8,30 +8,30 @@ import Root from "../app/Root"
 import State from "../app/State"
 import Config from "../app/Config.yml"
 
-/* eslint-disable no-console */
-export function start()
-{
-  var ssrData = {
-    apolloUri: "http://localhost:9123",
-    defaultLocale: Config.DEFAULT_LOCALE
-  }
 
-  const server = createExpressServer()
-
-  // Bind our universal react app middleware as the handler for all get requests.
-  server.get("*", createUniversalMiddleware({ Root, State, ssrData }))
-
-  // Add default handling for any remaining requests which are not catched by our middleware
-  addFallbackHandler(server)
-
-  // Start listening
-  server.listen(process.env.SERVER_PORT)
-
-  console.log(`Started React Server (Port: ${process.env.SERVER_PORT})`)
-}
+/*
+==================================================================
+  SERVER :: APPLICATION BOOTING SEQUENCE
+==================================================================
+*/
 
 // We want improved stack traces for NodeJS in both development and production
 enableEnhancedStackTraces()
 
-// Auto start server
-start()
+// Data to move from server to client
+var ssrData = {
+  apolloUri: "http://localhost:9123",
+  defaultLocale: Config.DEFAULT_LOCALE
+}
+
+// Create express server instance
+const server = createExpressServer()
+
+// Bind our universal middleware as the handler for all get requests.
+server.get("*", createUniversalMiddleware({ Root, State, ssrData }))
+
+// Add default handling for any remaining requests which are not catched by our middleware
+addFallbackHandler(server)
+
+// Start listening
+server.listen(process.env.SERVER_PORT)
