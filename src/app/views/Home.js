75895a44c97434839db702c8d8053ee554368eb6
Sebastian Werner <s.werner@sebastian-software.de>
2017-03-15 14:58:10 +0100

    Cleanup test code

--- src/app/views/Home.js
+++ src/app/views/Home.js
@@ -6,9 +6,6 @@ import Styles from "./Home.css"
 import Tweets from "../graphql/Tweets.gql"
 console.log("Loading GraphQL queries works:", Tweets.kind === "Document")
 
-import { format } from "date-fns"
-console.log("Today:", format(Date.now(), "DD.MM.YYYY"))
-
 function Home({ intl }) {
   return (
     <article>
